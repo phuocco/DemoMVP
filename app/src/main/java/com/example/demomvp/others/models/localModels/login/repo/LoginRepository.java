1041b45298d5c34b0703a9fed2ece0a2e79c750f
Phuoc Nguyen <phuocco2901@gmail.com>
2020-03-13 10:22:16 +0700

    clean

--- app/src/main/java/com/example/demomvp/others/models/localModels/login/repo/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demomvp.others.models.localModels.login.repo;
-
-import com.example.demomvp.others.models.localModels.login.User;
-
-public interface LoginRepository {
-    User getUser();
-
-    void saveUser(User user);
-}
